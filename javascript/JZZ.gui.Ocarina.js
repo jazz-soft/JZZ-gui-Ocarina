5e7f55626abb169fa93951a6faa5714eae154597
Sema <jazz-dev@jazz-soft.net>
2025-10-24 21:20:29 -0400

    dump()

--- javascript/JZZ.gui.Ocarina.js
+++ javascript/JZZ.gui.Ocarina.js
@@ -20,15 +20,26 @@
     this.y = y;
     this.r = r;
   }
-  const _dots = [[0, 0, 0.1]];
+  Dot.prototype.dump = function() {
+    return '<circle cx="' + this.x + '" cy="' + this.y + '" r="' + this.r + '"/>';
+  };
+  const _dots = [[0, 0, 0.1], [0.5, 0.5, 0.1], [1, 1, 0.1]];
 
   function Ocarina(w) {
-    var i;
+    var i, d;
     if (!w) w = {};
     if (!w.dots) w.dots = _dots;
     this.dots = [];
-    for (i = 0; i < w.dots.length; i++) this.dots.push();
+    for (i = 0; i < w.dots.length; i++) {
+      d = w.dots[i];
+      this.dots.push(new Dot(d[0], d[1], d[2]));
+    }
   }
+  Ocarina.prototype.dump = function(w, h) {
+    var svg = [];
+    for (i = 0; i < this.dots.length; i++) svg.push(this.dots[i].dump());
+    return svg.join('\n');
+  };
 
   JZZ.gui.Ocarina = Ocarina;
 });
---
+++ test.js
@@ -0,0 +1,10 @@
+const JZZ = require('jzz');
+require('jzz-gui-ocarina')(JZZ);
+
+var oca = new JZZ.gui.Ocarina();
+var w = 200, h = 200;
+var dump = [];
+dump.push('<svg version="1.1" width="' + w + '" height="' + h + '" viewBox="0 0 1 1" xmlns="http://www.w3.org/2000/svg">');
+dump.push(oca.dump());
+dump.push('</svg>');
+console.log(dump.join('\n'));
